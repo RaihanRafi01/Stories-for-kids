aba353146bc4681d5059bbc605abfc6f10cb353c
Raihan <r.rafi.aiub@gmail.com>
2024-02-29 19:49:26 +0600

    cleanup

--- app/src/main/java/com/example/stories/SplashScreenActivity.kt
+++ app/src/main/java/com/example/stories/SplashScreenActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
