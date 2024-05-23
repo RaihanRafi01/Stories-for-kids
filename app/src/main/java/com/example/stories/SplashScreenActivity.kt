3f0ac02c462dbe8e740872ef6aa71ad775ad2972
Raihan <r.rafi.aiub@gmail.com>
2024-05-23 22:00:58 +0600

    Splash changes

--- app/src/main/java/com/example/stories/SplashScreenActivity.kt
+++ app/src/main/java/com/example/stories/SplashScreenActivity.kt
@@ -3,17 +3,26 @@ package com.example.stories
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.view.animation.AnimationUtils
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.stories.databinding.ActivityMainBinding
+import com.example.stories.databinding.ActivitySplashScreenBinding
+
 class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val animation = AnimationUtils.loadAnimation(this, R.anim.logo_anim)
+        binding.imageView.startAnimation(animation)
+
         Handler().postDelayed({
             val iMain = Intent(this,MainActivity::class.java)
             startActivity(iMain)
             finish()
-        },3000)
+        },2000)
     }
 }
